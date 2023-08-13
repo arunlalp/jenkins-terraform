289cd6e3a65ef6291ebc468afa2280537d3ec854
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-08-13 18:26:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,5 @@ provider "aws" {
 
 resource "aws_instance" "myFirstInstance" {
   ami                    = var.ami_id
-  key_name               = var.key_name
   instance_type          = var.instance_type
-  tags= {
-    Name = var.tag_name
-  }
 }
