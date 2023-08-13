f0398a02efad420ddbde0f37f667ad66bd78168f
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-08-13 18:24:11 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "myFirstInstance" {
+  ami                    = var.ami_id
+  key_name               = var.key_name
+  instance_type          = var.instance_type
+  tags= {
+    Name = var.tag_name
+  }
+}
