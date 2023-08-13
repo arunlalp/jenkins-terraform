b3d69277b5b43abec912beac0ca8718b55a6db6e
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-08-13 18:27:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,5 +10,5 @@ variable "instance_type" {
 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0ee23bfc74a881de5" 
+    default     = "ami-03f65b8614a860c29" 
 }
