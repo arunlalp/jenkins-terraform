d93edfdc52a85f36ef7bbeab71e55d4112cd0292
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-08-18 22:51:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,6 +9,30 @@ variable "instance_type" {
 }
 
 variable "ami_id" { 
-    description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-03f65b8614a860c29" 
+  description = "AMI for Ubuntu Ec2 instance" 
+  default     = "ami-03f65b8614a860c29" 
+}
+
+variable "from_port" {
+  type        = list(number)
+  default     = [22]
+  description = "List of starting ports for cidr ingress rules of the EC2 security group."
+}
+
+variable "to_port" {
+  type        = list(number)
+  default     = [22]
+  description = "List of ending ports for cidr ingress rules of the EC2 security group."
+}
+
+variable "protocol" {
+  type        = list(string)
+  default     = ["tcp"]
+  description = "List of protocols for cidr ingress rules of the EC2 security group."
+}
+
+variable "cidr_block" {
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+  description = "List of CIDR blocks for cidr ingress rules of the EC2 security group."
 }
