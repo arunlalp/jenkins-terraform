832ad4a2896019350dbde67fc2b950e3e8aa4e24
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-08-18 22:48:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,4 +4,11 @@ provider "aws" {
 
 resource "aws_security_group" "instance_sg" {
   name        = "instance-sg"
+
+  ingress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = var.protocol
+    cidr_blocks      = var.cidr_block
+  }
 }
