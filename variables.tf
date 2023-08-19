95380d229792986e605f666506c3280aa973e744
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-08-19 18:11:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "aws_region" {
-       description = "The AWS region to create things in." 
-       default     = "us-west-2" 
+  description = "The AWS region to create things in."
+  default     = "us-west-2"
 }
 
-variable "instance_type" { 
-    description = "instance type for ec2" 
-    default     =  "t2.micro" 
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     = "t2.micro"
 }
 
-variable "ami_id" { 
-  description = "AMI for Ubuntu Ec2 instance" 
-  default     = "ami-03f65b8614a860c29" 
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-03f65b8614a860c29"
 }
 
 variable "from_port" {
@@ -26,7 +26,7 @@ variable "to_port" {
 }
 
 variable "protocol" {
-  type        = list
+  type        = list(string)  # Corrected data type to list of strings
   default     = ["tcp"]
   description = "List of protocols for cidr ingress rules of the EC2 security group."
 }
