fa84cd3381a79637c3d595017f1d2ef4e14b3832
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-04 15:16:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ resource "aws_security_group" "instance_sg" {
     from_port        = var.from_port
     to_port          = var.to_port
     protocol         = var.protocol
-    cidr_blocks      = var.cidr_blocks
+    cidr_blocks      = var.cidr_block
   }
 }
