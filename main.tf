5583f8b4b0546eabda83a6127766bd5bbec794b7
Arun Lal <118952274+arunlalp@users.noreply.github.com>
2023-09-04 15:15:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "instance_sg" {
   #description = "checkov test"
 
   ingress {
-    from_port        = var.form_port
+    from_port        = var.from_port
     to_port          = var.to_port
     protocol         = var.protocol
     cidr_blocks      = var.cidr_blocks
